8425fd9ed51c3f0beccdc17085853e0518e639dc
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2019-05-16 07:58:32 -0500

    add a fibonacci test

--- test.py
+++ test.py
@@ -2,3 +2,14 @@ import time
 
 def test_my_stuff(benchmark):
     benchmark(time.sleep, 0.10)
+
+
+def fib(a):
+    if a <= 0:
+        return 0
+    if a == 1:
+        return 1
+    return fib(a-1) + fib(a-2)
+
+def test_my_stuff(benchmark):
+    benchmark(fib, 35)
